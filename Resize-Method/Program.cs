2c3ed951777cdcba7e05307978eda6b9124a2051
Ulvi Asadov <148266987+ulviasadovv@users.noreply.github.com>
2023-11-01 15:41:07 +0400

    Add files via upload

---
+++ Resize-Method/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[5];
+
+int b = 0;
+int newsize(ref int[] arr, int b)
+{
+    if (b < 0)
+    {
+        int[] newarr = [arr.length - b];
+        return newarr;
+    }
+    else
+    {
+        int[] newarr = [arr.length + b];
+        return newarr;
+    };
+};
